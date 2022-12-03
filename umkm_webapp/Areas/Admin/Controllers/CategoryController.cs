dad35a294357663a7f7805dd8ccacb6c26c88d0c
DrugFFa <articuno712@gmail.com>
2022-12-04 02:49:42 +0700

    Add Category Controller 2

--- umkm_webapp/Areas/Admin/Controllers/CategoryController.cs
+++ umkm_webapp/Areas/Admin/Controllers/CategoryController.cs
@@ -1,15 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using umkm_webapp.Models;
 
 namespace umkm_webapp.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin")]
+    [Area("admin")]
+    [Route("admin/category")]
     public class CategoryController : Controller
     {
+        private DatabaseContext db = new DatabaseContext();
+
+        public CategoryController(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
+        [Route("")]
+        [Route("index")]
         public IActionResult Index()
         {
+            ViewBag.categories = db.Categories.ToList();
             return View();
         }
     }
--- umkm_webapp/Models/Role.cs
+++ umkm_webapp/Models/Role.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -7,6 +8,7 @@ using System.Collections.Generic;
 
 namespace umkm_webapp.Models
 {
+    [Table("Role")]
     public partial class Role
     {
         public Role()
