c2cf92bb33a09ec1c628f6a2ad3b3b329c5aa5bc
cahyooo <toriqzuhudnurcahyo@gmail.com>
2022-12-04 23:11:43 +0700

    add categories, view component product categories

---
+++ umkm_webapp/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using umkm_webapp.Models;
+
+namespace umkm_webapp.ViewComponents
+{
+    [ViewComponent(Name = "Category")]
+    public class CategoryViewComponent : ViewComponent
+    {
+        private DatabaseContext db;
+        public CategoryViewComponent(DatabaseContext _db)
+        {
+            this.db = _db;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Category> categories = db.Categories.Where(c => c.Status && c.Parent == null).ToList();
+            return View("Index", categories);
+        }
+    }
+}
