cb8d9579ee5b72fb9274afe6564081a9a6d35e3c
DrugFFa <68659848+DrugFFa@users.noreply.github.com>
2022-12-07 19:47:52 +0700

    Adding Edit in ProductController

--- umkm_webapp/Areas/Admin/Controllers/ProductController.cs
+++ umkm_webapp/Areas/Admin/Controllers/ProductController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using umkm_webapp.Areas.Admin.Models.ViewModels;
 using umkm_webapp.Models;
 
 namespace umkm_webapp.Areas.Admin.Controllers
@@ -30,8 +32,110 @@ namespace umkm_webapp.Areas.Admin.Controllers
             ViewBag.Products = db.Products.ToList();
             return View();
         }
+
+        [HttpGet]
+        [Route("Add")]
+        public IActionResult Add()
+        {
+            var productViewModel = new ProductViewModel();
+            productViewModel.Product = new Product();
+            productViewModel.Categories = new List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem>();
+
+            var categories = db.Categories.ToList();
+            foreach (var category in categories)
+            {
+                var group = new SelectListGroup { Name = category.Name };
+                if (category.InverseParents != null && category.InverseParents.Count > 0)
+                    foreach (var subCategory in category.InverseParents)
+                    {
+                        var selectListItem = new SelectListItem
+                        {
+                            Text = subCategory.Name,
+                            Value = subCategory.Id.ToString(),
+                            Group = group
+                        };
+                        productViewModel.Categories.Add(selectListItem);
+                    }
+            }
+
+            return View("Add", productViewModel);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Add(ProductViewModel productViewModel)
+        {
+            db.Products.Add(productViewModel.Product);
+            db.SaveChanges();
+
+            var defaultPhoto = new Photo
+            {
+                Name = "no-image.jpg",
+                Status = true,
+                ProductId = productViewModel.Product.Id,
+                Featured = true
+            };
+            db.Photos.Add(defaultPhoto);
+            db.SaveChanges();
+            return RedirectToAction("Index", "product", new { area = "admin" });
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var product = db.Products.Find(id);
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return RedirectToAction("Index", "product", new { area = "admin" });
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public IActionResult Edit(int id)
+        {
+
+            var productViewModel = new ProductViewModel();
+            productViewModel.Product = db.Products.Find(id);
+            productViewModel.Categories = new List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem>();
+
+            var categories = db.Categories.ToList();
+            foreach (var category in categories)
+            {
+                var group = new SelectListGroup { Name = category.Name };
+                if (category.InverseParents != null && category.InverseParents.Count > 0)
+                    foreach (var subCategory in category.InverseParents)
+                    {
+                        var selectListItem = new SelectListItem
+                        {
+                            Text = subCategory.Name,
+                            Value = subCategory.Id.ToString(),
+                            Group = group
+                        };
+                        productViewModel.Categories.Add(selectListItem);
+                    }
+            }
+
+            return View("Edit", productViewModel);
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        public IActionResult Edit(int id,ProductViewModel productViewModel)
+        {
+            db.Entry(productViewModel.Product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "product", new { area = "admin" });
+        }
+
     }
 }
-
-
-
---
+++ umkm_webapp/Areas/Admin/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using umkm_webapp.Models;
+
+namespace umkm_webapp.Areas.Admin.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+
+        public List<SelectListItem> Categories { get; set; }
+    }
+}
