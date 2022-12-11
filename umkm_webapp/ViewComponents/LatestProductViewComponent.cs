6bafff83c1b090312f26753afc33f3b9ef812828
Toriq Zuhud Nurcahyo <toriqzuhudnurcahyo@gmail.com>
2022-12-12 00:52:19 +0700

    add product controller user,add edit and delete photocontroller admin, add product featured viw, add product latest

---
+++ umkm_webapp/ViewComponents/LatestProductViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using umkm_webapp.Models;
+
+namespace umkm_webapp.ViewComponents
+{
+    [ViewComponent(Name = "LatestProduct")]
+    public class LatestProductViewComponent : ViewComponent
+    {
+        private DatabaseContext db;
+        public LatestProductViewComponent(DatabaseContext _db)
+        {
+            this.db = _db;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Product> products = db.Products.OrderByDescending(p => p.Id).Where(p => p.Status).Take(2).ToList();
+            return View("Index", products);
+        }
+    }
+}
