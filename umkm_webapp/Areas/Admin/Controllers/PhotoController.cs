7840ac6974c9d3720c382fcbe19d735802a2c110
DrugFFa <68659848+DrugFFa@users.noreply.github.com>
2022-12-11 22:00:26 +0700

    Add Edit+Delete+Featured in PhotoController

--- umkm_webapp/Areas/Admin/Controllers/PhotoController.cs
+++ umkm_webapp/Areas/Admin/Controllers/PhotoController.cs
@@ -61,5 +61,61 @@ namespace umkm_webapp.Areas.Admin.Controllers
             db.SaveChanges();
             return RedirectToAction("index", "photo", new { area = "admin", id = productid });
         }
+
+        [HttpGet]
+        [Route("delete/{id}/productId")]
+        public IActionResult Delete(int id, int productId)
+        {
+            var photo = db.Photos.Find(id);
+            db.Photos.Remove(photo);
+            db.SaveChanges();
+            return RedirectToAction("index", "photo", new { area = "admin", id = productId });
+        }
+
+        [HttpGet]
+        [Route("edit/{id}/{productId}")]
+        public IActionResult Edit(int id, int productId)
+        {
+            ViewBag.Product = db.Products.Find(productId);
+            var photo = db.Photos.Find(id);
+            return View("Edit", photo);
+        }
+
+        [HttpPost]
+        [Route("edit/{photoId}/{productId}")]
+        public IActionResult Edit(int photoId, int productId, Photo photo, IFormFile fileUpload)
+        {
+            var currentPhoto = db.Photos.Find(photo.Id);
+            if (fileUpload != null && !string.IsNullOrEmpty(fileUpload.FileName))
+            {
+                var fileName = DateTime.Now.ToString("MMddyyyyhhmmss") + fileUpload.FileName;
+                var path = Path.Combine(this.ihostingEnvironment.WebRootPath, "product", fileName);
+                var stream = new FileStream(path, FileMode.Create);
+                fileUpload.CopyToAsync(stream);
+                currentPhoto.Name = fileName;
+            }
+            currentPhoto.Status = photo.Status;
+            currentPhoto.Featured = photo.Featured;
+            db.SaveChanges();
+            return RedirectToAction("index", "photo", new { area = "admin", id = productId });
+
+        }
+
+        [HttpGet]
+        [Route("SetFeatured/{id}/{productId}")]
+        public IActionResult SetFeatured(int id, int productId)
+        {
+            var product = db.Products.Find(productId);
+            product.Photos.ToList().ForEach(p =>
+            {
+                p.Featured = false;
+                db.Entry(p).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                db.SaveChanges();
+            });
+            var photo = db.Photos.Find(id);
+            photo.Featured = true;
+            db.SaveChanges();
+            return RedirectToAction("index", "photo", new { area = "admin", id = productId });
+        }
     }
 }
