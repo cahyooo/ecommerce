4f3708ea362314020de489bc770b4252c0805fe0
Toriq Zuhud Nurcahyo <toriqzuhudnurcahyo@gmail.com>
2022-12-17 00:56:19 +0700

    add dashboard user, add customer controler in admin,add  update data customer view

--- umkm_webapp/ViewComponents/LatestProductViewComponent.cs
+++ umkm_webapp/ViewComponents/LatestProductViewComponent.cs
@@ -6,16 +6,19 @@ using umkm_webapp.Models;
 
 namespace umkm_webapp.ViewComponents
 {
-    [ViewComponent(Name = "Login")]
-    public class LoginViewComponent : ViewComponent
+    [ViewComponent(Name = "LatestProduct")]
+    public class LatestProductViewComponent : ViewComponent
     {
-        
-
+        private DatabaseContext db;
+        public LatestProductViewComponent(DatabaseContext _db)
+        {
+            this.db = _db;
+        }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            
-            return View("Login");
+            List<Product> products = db.Products.OrderByDescending(p => p.Id).Where(p => p.Status).Take(2).ToList();
+            return View("Index", products);
         }
     }
 }
