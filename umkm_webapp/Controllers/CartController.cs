40d2ddc3caf1dfd98372b596705f75fa25fd5c22
DrugFFa <68659848+DrugFFa@users.noreply.github.com>
2022-12-20 15:43:30 +0700

    Adding Cart Controller

--- umkm_webapp/Controllers/CartController.cs
+++ umkm_webapp/Controllers/CartController.cs
@@ -3,14 +3,87 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using umkm_webapp.Helpers;
+using umkm_webapp.Models;
 
 namespace umkm_webapp.Controllers
 {
+    [Route("cart")]
     public class CartController : Controller
     {
+        private DatabaseContext db = new DatabaseContext();
+
+        public CartController(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
+        [Route("")]
+        [Route("index")]
         public IActionResult Index()
         {
+            List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            ViewBag.cart = cart;
+            ViewBag.countItems = cart.Count;
+            ViewBag.Total = cart.Sum(it => it.Price * it.Quantity);
             return View();
+        }
+
+        [Route("buy/{id}")]
+        public IActionResult Buy(int id)
+        {
+            var product = db.Products.Find(id);
+            var photo = product.Photos.SingleOrDefault(ph => ph.Status && ph.Featured);
+            var photoName = photo == null ? "no-image.png" : photo.Name;
+            if (SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart") == null)
+            {
+                var cart = new List<Item>();
+                cart.Add(new Item
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Price = product.Price,
+                    Photo = photoName,
+                    Quantity = 1
+                });
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
+            }
+            else
+            {
+               var cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+                int index = exists(id, cart);
+                if (index == -1)
+                {
+                    cart.Add(new Item
+                    {
+                        Id = product.Id,
+                        Name = product.Name,
+                        Price = product.Price,
+                        Photo = photoName,
+                        Quantity = 1
+                    });
+                }
+                else
+                {
+                    cart[index].Quantity++;
+                    
+                }
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
+
+            }
+            return RedirectToAction("Index","Cart");
+        }
+
+        private int exists(int id, List<Item> cart)
+        {
+            for (var i = 0; i < cart.Count; i++)
+            {
+                if (cart[i].Id == id)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }
--- umkm_webapp/ViewComponents/CartTopViewComponent.cs
+++ umkm_webapp/ViewComponents/CartTopViewComponent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using umkm_webapp.Helpers;
 using umkm_webapp.Models;
 
 namespace umkm_webapp.ViewComponents
@@ -9,15 +10,23 @@ namespace umkm_webapp.ViewComponents
     [ViewComponent(Name = "CartTop")]
     public class CartTopViewComponent : ViewComponent
     {
-        private DatabaseContext db;
-        public CartTopViewComponent(DatabaseContext _db)
-        {
-            this.db = _db;
-        }
-
+        
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            if (SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart") == null)
+            {
+                ViewBag.countItems = 0;
+                ViewBag.Total = 0;
+            }
+
+            else
+            {
+                List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+                ViewBag.countItems = cart.Count;
+                ViewBag.Total = cart.Sum(it => it.Price * it.Quantity);
+            }
+            
             return View("Index");
         }
 
